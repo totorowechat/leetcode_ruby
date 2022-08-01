b49c79a0ba62cb33947d115aafed2d6c61875376
Lingao Jin <jinlingao@outlook.com>
2022-08-01 20:19:40 +0800

    feat: problem 58

--- lib/leetcode_ruby/0058_length_of_last_word.rb
+++ lib/leetcode_ruby/0058_length_of_last_word.rb
@@ -2,6 +2,18 @@
 
 module LeetcodeRuby
   module LengthOfLastWord
-  
+    # @param {String} s
+    # @return {Integer}
+    def length_of_last_word(s)
+      s = s.strip
+      index = s.size - 1
+      count = 0
+      s.size.times do
+        return count if s[index] == ' '
+
+        count += 1
+        index -= 1
+      end
+    end
   end
 end
--- spec/0058_length_of_last_word_spec.rb
+++ spec/0058_length_of_last_word_spec.rb
@@ -5,6 +5,7 @@ require 'leetcode_ruby'
 RSpec.describe LeetcodeRuby::LengthOfLastWord do
   let(:instance) { Object.new.extend LeetcodeRuby::LengthOfLastWord }
   it '' do
-    expect(instance).to eq()
+    input = 'Hello World'
+    expect(instance.length_of_last_word(input)).to eq(5)
   end
 end
